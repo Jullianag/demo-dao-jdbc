eb3e7cb9582a14b2a81f590f5e92d22e2ef38c16
Julliana <juh_g@hotmail.com>
2024-02-18 15:31:18 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,13 +12,16 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        //Department obj = new Department(1, "Books");
+        //System.out.println(obj);
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
-        System.out.println(seller);
+        //Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        //System.out.println(seller);
 
         // o programa conhece somente a interface
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -94,7 +94,7 @@ public class Seller implements Serializable {
 
     @Override
     public String toString() {
-        return "Seller{" +
+        return "Seller {" +
                 "id: " + id +
                 ", name: " + name
                 + ", email: " + email
